98b8b0d6838af0dd8574df55c68414b85840a542
Said-ob7 <saideloubid2@gmail.com>
2024-07-08 15:41:19 +0100

    Initial Commit

---
+++ GestionBackend/src/main/java/cih/ma/gestionbackend/GestionBackendApplication.java
@@ -0,0 +1,13 @@
+package cih.ma.gestionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBackendApplication.class, args);
+    }
+
+}
---
+++ GestionBackend/src/test/java/cih/ma/gestionbackend/GestionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cih.ma.gestionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
