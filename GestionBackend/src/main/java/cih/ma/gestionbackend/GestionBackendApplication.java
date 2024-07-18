ae7d3a793e8cb64ec7282567401f034cbb0c9abd
othmane <othmanelazrak031@gmail.com>
2024-07-18 15:15:33 +0100

    initial commit

--- GestionBackend/src/main/java/cih/ma/gestionbackend/GestionBackendApplication.java
+++ GestionBackend/src/main/java/cih/ma/gestionbackend/GestionBackendApplication.java
@@ -1,11 +1,11 @@
 package cih.ma.gestionbackend;
 
+import cih.ma.gestionbackend.Repository.BonC_repository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GestionBackendApplication.class, args);
     }
--- GestionFrontend/src/components/Commandes.tsx
+++ GestionFrontend/src/components/Commandes.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import FileUpload from "@/components/BC/FileUpload.tsx";
+import FileList from "@/components/BC/FileList.tsx";
 
 function Commandes() {
-  return <div>Commandes</div>;
+  return (
+      <>
+      <div>Commandes</div>
+
+<FileList />
+         <div> <FileUpload /></div>
+  </>
+  );
 }
 
 export default Commandes;
